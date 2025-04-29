519e3fe261add449d1baf256ae80099454d68f6e
Raul <rap.gardini@gmail.com>
2025-04-29 12:17:06 -0300

    feat: adicionando linkedin e texto com imagem no meio da tela inicial

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom'
 import { AiFillGithub } from "react-icons/ai";
+import { FaLinkedin } from "react-icons/fa";
 import './Footer.css'
 
 function Footer() {
@@ -16,6 +17,13 @@ function Footer() {
                 <Link to="/" style={{marginLeft: "40px"}}>Projetos</Link>
             </div>
             <div className="github">
+                <a 
+                href="https://www.linkedin.com/in/raul-gardini-2595712b1/" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                >
+                    <FaLinkedin />
+                </a>
                 <a 
                 href="https://github.com/RaulGardini" 
                 target="_blank" 
