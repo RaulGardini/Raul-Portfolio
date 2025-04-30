af9c9b5fce3a3db178810139a8a3d4918c6ca382
Raul <rap.gardini@gmail.com>
2025-04-30 11:34:13 -0300

    feat: criando a pagina de linguagens

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -6,31 +6,31 @@ import './Footer.css'
 
 function Footer() {
 
-    return(
+    return (
         <>
-        <footer>
-            <div className="links">
-                <Link to="/">Inicio</Link>
-                <Link to="/Linguagens" style={{marginLeft: "40px"}}>Linguagens</Link>
-                <Link to="/Projetos" style={{marginLeft: "40px"}}>Projetos</Link>
-            </div>
-            <div className="github">
-                <a 
-                href="https://www.linkedin.com/in/raul-gardini-2595712b1/" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                >
-                    <FaLinkedin />
-                </a>
-                <a 
-                href="https://github.com/RaulGardini" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                >
-                    <AiFillGithub />
-                </a>
-            </div>
-        </footer>
+            <footer>
+                <div className="links">
+                    <Link to="/">Inicio</Link>
+                    <Link to="/Linguagens" style={{ marginLeft: "40px" }}>Linguagens</Link>
+                    <Link to="/Projetos" style={{ marginLeft: "40px" }}>Projetos</Link>
+                </div>
+                <div className="github">
+                    <a
+                        href="https://www.linkedin.com/in/raul-gardini-2595712b1/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <FaLinkedin />
+                    </a>
+                    <a
+                        href="https://github.com/RaulGardini"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <AiFillGithub />
+                    </a>
+                </div>
+            </footer>
         </>
     )
 }
--- src/paginas/inicio/Inicio.jsx
+++ src/paginas/inicio/Inicio.jsx
@@ -3,22 +3,22 @@ import Footer from '../../componentes/Footer.jsx'
 import './Inicio.css'
 
 function Inicio() {
-    return(
+    return (
         <>
-        <main>
-            <Footer/>
-            <div className="resumo">
-                <div className="imagem">
-                    <img src="/Raul.jpeg" alt="Foto de Raul" />
+            <main>
+                <Footer />
+                <div className="resumo">
+                    <div className="imagem">
+                        <img src="/Raul.jpeg" alt="Foto de Raul" />
+                    </div>
+                    <div className="texto">
+                        <h2>Raul Gardini</h2>
+                        <p>Desenvolvedor Full Stack Jr com foco em construção de interfaces responsivas e integradas com back-end. Tenho experiência em projetos utilizando HTML, CSS, JavaScript, TypeScript, C#, ReactJS e React Native.</p>
+                        <p>No back-end, atuo na BitCodeHouse com C# e .NET, criando APIs robustas e seguras para o sistema Delphos, utilizado pela Solis Investimentos. Também tenho conhecimento em  ferramentas como Git e GitHub.</p>
+                        <p>Estou em constante aprendizado, buscando sempre novas tecnologias e aprimoramento técnico. Gosto de desafios e de transformar ideias em soluções.</p>
+                    </div>
                 </div>
-                <div className="texto">
-                    <h2>Raul Gardini</h2>
-                    <p>Desenvolvedor Full Stack Jr com foco em construção de interfaces responsivas e integradas com back-end. Tenho experiência em projetos utilizando HTML, CSS, JavaScript, TypeScript, C#, ReactJS e React Native.</p>
-                    <p>No back-end, atuo na BitCodeHouse com C# e .NET, criando APIs robustas e seguras para o sistema Delphos, utilizado pela Solis Investimentos. Também tenho conhecimento em  ferramentas como Git e GitHub.</p>
-                    <p>Estou em constante aprendizado, buscando sempre novas tecnologias e aprimoramento técnico. Gosto de desafios e de transformar ideias em soluções.</p>
-                </div>
-            </div>
-        </main>
+            </main>
         </>
     )
 }
